023111320915318a6b5e7df22500c67d2c400d0d
Arul <aprprasanth16@gmail.com>
2020-04-06 17:05:58 -0400

    changes

--- src/pages/checkout-page/CheckoutPage.js
+++ src/pages/checkout-page/CheckoutPage.js
@@ -49,7 +49,7 @@ return (
     <div className='test-warning'>
       *Please use the following test credit card for payments*
       <br />
-      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+      4242 4242 4242 4242 - Exp: 04/25 - CVV: 123
     </div>
     {/* <StripeCheckoutButton price={total} /> */}
   </div>
