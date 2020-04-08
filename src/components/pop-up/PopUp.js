2c23a57b090945265b559558f00e0926c5ab0687
Arul <aprprasanth16@gmail.com>
2020-04-08 13:24:54 -0400

    changes

---
+++ src/components/pop-up/PopUp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const PopUp = () => {
+    return (
+    <div className='popup'>  
+        <div className='popup\_inner'>  
+             <h1>{"hey"}</h1>  
+            <button onClick={"close"}>close me</button>  
+        </div>  
+    </div> 
+)
+}
